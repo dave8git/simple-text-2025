0c846c47417380f3a049adebc9568febdf0e14a6
dave8git <dave8git@gmail.com>
2025-09-28 04:32:54 +0200

    Add manual save and auto save works

--- renderer.js
+++ renderer.js
@@ -3,43 +3,57 @@ editor.textContent = "Type away Bro..." // it's not textbox just div - so to get
 
 let currentFilePath = null;
 
-document.addEventListener('keydown', 
+document.addEventListener('keydown',
     deBounce(keydownAction, 100)
 )
 
-editor.addEventListener("input", 
+editor.addEventListener("input",
     deBounce(saveFile, 1000)
 )
 
 async function keydownAction(event) {
-        if (event.ctrlKey && event.key.toLowerCase() === 'o') {
-            event.preventDefault();
-            await openFile();
-        }
-        if (event.ctrlKey && event.key.toLowerCase() === 's') {
-            event.preventDefault();
-            const result = await window.electronAPI.manualSave(editor.textContent);
-            if (result) {
-                currentFilePath = result;
-                console.log("File manually saved:", result);
-            }
-        }
+    if (event.ctrlKey && event.key.toLowerCase() === 'o') {
+        event.preventDefault();
+        await openFile();
+    }
+    if (event.ctrlKey && event.key.toLowerCase() === 's') {
+        event.preventDefault();
+        await saveFile();
+    }
 }
 
 async function openFile() {
-    currentFilePath = await window.electronAPI.openFile();
-    console.log('content', currentFilePath);
-    editor.textContent = currentFilePath.content;
+    const result = await window.electronAPI.openFile(); // funkcja open file zwraca obiekt: { success: true, path: filePaths[0], content }
+    if(result && result.success) {
+        editor.textContent = result.content;
+        currentFilePath = result.path;
+    }
+    
 }
 
 async function saveFile() {
-    console.log('autoSave')
     const data = editor.textContent;
-    await window.electronAPI.autoSave(currentFilePath, data);
+
+    if (!currentFilePath) {
+        const result = await window.electronAPI.manualSave(data);
+        if (result) {
+            currentFilePath = result;
+            return result;
+        } else {
+            return null;
+        }
+    } else {
+        const result = await window.electronAPI.autoSave(currentFilePath, data);
+        if (result) {
+            return result;
+        } else {
+            return null;
+        }
+    }
 }
 
 function deBounce(cb, delay) {
-    let stId; 
+    let stId;
     return (...args) => {
         clearTimeout(stId);
         stId = setTimeout(() => cb(...args), delay)  // macroTask - funkcja wykona się na końcu kolejki
